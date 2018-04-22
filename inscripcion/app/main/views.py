8fe81aa0736f73f325ef8a6fea11d8edb4236f7f
faustocv <fausto.castaneda.v@gmail.com>
2018-04-22 17:01:00 -0500

    Adds footer include file

--- inscripcion/app/main/views.py
+++ inscripcion/app/main/views.py
@@ -1,6 +1,13 @@
 from flask import render_template
 from . import main
 
+site = {
+        'title': 'Encuentro Carismático Católico Ecuatoriano Juvenil 2018',
+        'description': 'XXXII Encuentro Carismático Católico Ecuatoriano Juvenil Quito 2018',
+        'facebook': 'https://www.facebook.com/eccejuquito2018',
+        'instagram': 'https://www.instagram.com/eccejuquito'
+        }
+
 @main.route('/formulario', methods = ['GET'])
 def formulario():
-    return render_template('formulario.html'), 200
+    return render_template('formulario.html', site = site), 200
