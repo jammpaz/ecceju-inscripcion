f591ab5744de8135f7f70ccac6836d4014b891b1
Fausto Castaneda <faustoc@ip-10-71-135-15.us-east-2.compute.internal>
2018-04-24 19:37:47 -0500

    Refines form style and adds a new button

--- inscripcion/app/main/forms.py
+++ inscripcion/app/main/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DecimalField
+from wtforms import StringField, SubmitField, DecimalField, FileField
 
 class InscripcionForm(FlaskForm):
     localidad = StringField('Nombre de la Localidad')
     servidor = StringField('Nombre del servidor/a')
     monto = DecimalField('Monto cancelado (USD)')
-    comprobante_uri = StringField('Comprobante de pago')
+    comprobante_uri = FileField('Comprobante de pago')
     guardar = SubmitField('Guardar')
+    enviar = SubmitField('Enviar')
 
